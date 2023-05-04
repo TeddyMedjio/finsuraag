87536172db9cde94a093a2e539fdaf6522775478
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2023-05-04 06:55:11 +0100

    update slide

--- src/components/Swiper/Slider.jsx
+++ src/components/Swiper/Slider.jsx
@@ -1,6 +1,9 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import Link from "next/link";
+import Image from "next/image";
+import family from "../../../public/img/family.jpg";
+import family2 from "../../../public/img/family2.jpg";
 
 // Import Swiper styles
 import "swiper/css";
@@ -24,7 +27,7 @@ export default function Slider() {
         modules={[Autoplay, Pagination, Navigation]}
         className="swiper"
       >
-        <SwiperSlide className="family relative z-40">
+        <SwiperSlide className=" z-40">
           <div className="relative container mx-auto z-[45]">
             <div className=" max-w-[500px] absolute left-0 top-0 mt-[50%] lg:mt-[20%] text-center lg:text-left">
               <h1 className="text-brand-blue font-bold text-5xl">
@@ -41,12 +44,13 @@ export default function Slider() {
               </Link>
             </div>
           </div>
-          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-70 z-[41] lg:hidden"></div>
+          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-70 z-[41] md:hidden"></div>
+          <Image src={family} height="auto" alt="image d'une famille" />
         </SwiperSlide>
 
-        <SwiperSlide className="family2 relative z-40">
+        <SwiperSlide className="family2 z-40">
           <div className="relative container mx-auto z-[45]">
-            <div className="  max-w-[550px] absolute right-0 top-0 mt-[50%] lg:mt-[20%] text-center lg:text-left  ">
+            <div className="max-w-[550px] md:max-w-full lg:max-w-[550px] absolute right-0 top-0 mt-[50%] lg:mt-[20%] text-center lg:text-left  ">
               <h1 className="text-brand-blue font-bold text-5xl">
                 Schutz im Alltag
               </h1>
@@ -61,7 +65,8 @@ export default function Slider() {
               </Link>
             </div>
           </div>
-          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-70 z-[41] lg:hidden"></div>
+          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-40 z-[41] md:hidden"></div>
+          <Image src={family2} height="auto" alt="image d'une famille" />
         </SwiperSlide>
       </Swiper>
     </>
