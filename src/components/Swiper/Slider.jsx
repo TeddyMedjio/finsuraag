337ea0cb12cecceff4ecb90812cb3db67789cfd3
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2023-05-04 07:42:08 +0100

    update font size

--- src/components/Swiper/Slider.jsx
+++ src/components/Swiper/Slider.jsx
@@ -29,43 +29,43 @@ export default function Slider() {
       >
         <SwiperSlide className=" z-40">
           <div className="relative container mx-auto z-[45]">
-            <div className=" max-w-[500px] absolute left-0 top-0 mt-[50%] lg:mt-[20%] text-center lg:text-left">
-              <h1 className="text-brand-blue font-bold text-5xl">
+            <div className=" max-w-[550px] md:max-w-full lg:max-w-[550px] absolute left-0 top-0 mt-[20%] md:mt-[40%] lg:mt-[15%] text-center lg:text-left">
+              <h1 className="text-brand-blue font-bold text-[56px]">
                 Finanzielle Sicherheit
               </h1>
-              <p className=" text-4xl text-black mt-5 mb-10">
+              <p className=" text-[40px] text-black mt-5 mb-10">
                 Sichere dich und deine Familie für unsichere Zeiten ab
               </p>
               <Link
                 href="/"
-                className="font-bold uppercase transition-all duration-300 ease bg-brand-blue hover:bg-brand-red text-white py-3 px-3 hover:px-7 rounded cursor-pointer tracking-widest "
+                className="text-xl font-bold uppercase transition-all duration-300 ease bg-brand-blue hover:bg-brand-red text-white py-3 px-3 hover:px-7 rounded cursor-pointer tracking-widest "
               >
                 Mehr erfahren
               </Link>
             </div>
           </div>
-          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-70 z-[41] md:hidden"></div>
+          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-70 z-[41] lg:hidden"></div>
           <Image src={family} height="auto" alt="image d'une famille" />
         </SwiperSlide>
 
         <SwiperSlide className="family2 z-40">
           <div className="relative container mx-auto z-[45]">
-            <div className="max-w-[550px] md:max-w-full lg:max-w-[550px] absolute right-0 top-0 mt-[50%] lg:mt-[20%] text-center lg:text-left  ">
-              <h1 className="text-brand-blue font-bold text-5xl">
+            <div className="max-w-[550px] md:max-w-full lg:max-w-[650px] absolute right-0 top-0 mt-[20%] md:mt-[40%] lg:mt-[15%] text-center lg:text-left  ">
+              <h1 className="text-brand-blue font-bold text-[56px]">
                 Schutz im Alltag
               </h1>
-              <p className=" text-4xl text-black mt-5 mb-10">
+              <p className=" text-[40px] text-black mt-5 mb-10">
                 Schütze dich und deine Gegenstände vor Risiken im Alltag
               </p>
               <Link
                 href="/"
-                className="font-bold uppercase transition-all duration-300 ease bg-brand-blue hover:bg-brand-red text-white py-3 px-3 hover:px-7 rounded cursor-pointer tracking-widest "
+                className="text-xl font-bold uppercase transition-all duration-300 ease bg-brand-blue hover:bg-brand-red text-white py-3 px-3 hover:px-7 rounded cursor-pointer tracking-widest "
               >
                 Mehr erfahren
               </Link>
             </div>
           </div>
-          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-40 z-[41] md:hidden"></div>
+          <div className="absolute left-0 top-0 bg-white w-full h-full opacity-40 z-[41] lg:hidden"></div>
           <Image src={family2} height="auto" alt="image d'une famille" />
         </SwiperSlide>
       </Swiper>
